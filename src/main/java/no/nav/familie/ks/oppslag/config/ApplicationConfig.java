472f3446b318b025c259e33945cdbcba53fe6a6b
Martine Enger <marenger@student.matnat.uio.no>
2019-09-09 09:42:49 +0200

    Tar i bruk log-artifakt fra familie-felles

--- src/main/java/no/nav/familie/ks/oppslag/config/ApplicationConfig.java
+++ src/main/java/no/nav/familie/ks/oppslag/config/ApplicationConfig.java
@@ -1,17 +1,13 @@
 package no.nav.familie.ks.oppslag.config;
 
-import no.nav.log.LogFilter;
 import org.ehcache.CacheManager;
 import org.ehcache.config.ResourcePools;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.expiry.ExpiryPolicy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -24,8 +20,6 @@ import static org.ehcache.config.builders.CacheConfigurationBuilder.newCacheConf
 @ComponentScan({ "no.nav.familie.ks.oppslag" })
 public class ApplicationConfig {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ApplicationConfig.class);
-
     @Bean
     ServletWebServerFactory servletWebServerFactory() {
 
@@ -34,15 +28,6 @@ public class ApplicationConfig {
         serverFactory.setPort(8085);
 
         return serverFactory;
-    }
-
-    @Bean
-    public FilterRegistrationBean<LogFilter> logFilter() {
-        LOG.info("Registering LogFilter filter");
-        final FilterRegistrationBean<LogFilter> filterRegistration = new FilterRegistrationBean<>();
-        filterRegistration.setFilter(new LogFilter());
-        filterRegistration.setOrder(1);
-        return filterRegistration;
     }
 
     @Bean
