8fe87a128cb9afee4a47c1d9132e790f07f7303c
Martine Enger <marenger@student.matnat.uio.no>
2019-09-10 09:23:48 +0200

    Revert "Tar i bruk log-artifakt fra familie-felles"
    This reverts commit 472f3446b318b025c259e33945cdbcba53fe6a6b.

--- src/main/java/no/nav/familie/ks/oppslag/config/ApplicationConfig.java
+++ src/main/java/no/nav/familie/ks/oppslag/config/ApplicationConfig.java
@@ -1,13 +1,17 @@
 package no.nav.familie.ks.oppslag.config;
 
+import no.nav.log.LogFilter;
 import org.ehcache.CacheManager;
 import org.ehcache.config.ResourcePools;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.expiry.ExpiryPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,6 +24,8 @@ import static org.ehcache.config.builders.CacheConfigurationBuilder.newCacheConf
 @ComponentScan({ "no.nav.familie.ks.oppslag" })
 public class ApplicationConfig {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationConfig.class);
+
     @Bean
     ServletWebServerFactory servletWebServerFactory() {
 
@@ -28,6 +34,15 @@ public class ApplicationConfig {
         serverFactory.setPort(8085);
 
         return serverFactory;
+    }
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> logFilter() {
+        LOG.info("Registering LogFilter filter");
+        final FilterRegistrationBean<LogFilter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new LogFilter());
+        filterRegistration.setOrder(1);
+        return filterRegistration;
     }
 
     @Bean
