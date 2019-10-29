152a03f3abd4f4f6fd3992a1f4da04c18bca9d3f
tnarland <47184872+tnarland@users.noreply.github.com>
2019-10-29 11:43:42 +0100

    Legge til feilhåndtering ved bruk av rest template: (#69)
    * Legge til feilhåndtering ved bruk av rest template:
    
    * legger til litt ekstra på loggutskriftene ved RestClientException
    
    * Endret response til json

--- src/main/java/no/nav/familie/ks/oppslag/config/ApiExceptionHandler.java
+++ src/main/java/no/nav/familie/ks/oppslag/config/ApiExceptionHandler.java
@@ -4,15 +4,16 @@ import no.nav.security.spring.oidc.validation.interceptor.OIDCUnauthorizedExcept
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Map;
+
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -20,24 +21,29 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger secureLogger = LoggerFactory.getLogger("secureLogger");
     private final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
-    @ExceptionHandler({OIDCUnauthorizedException.class, HttpClientErrorException.Unauthorized.class})
-    public ResponseEntity<String> handleUnauthorizedException() {
-        logger.warn("Kan ikke behandle pga. bruker ikke logget inn.");
-        return ResponseEntity.status(UNAUTHORIZED).body("Du er ikke logget inn");
+    public ApiExceptionHandler() {
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler({OIDCUnauthorizedException.class})
+    public Map<String, String> handleUnauthorizedException(OIDCUnauthorizedException e) {
+        logger.warn("Kan ikke logget inn.", e);
+        return Map.of("error", "Du er ikke logget inn");
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler({RestClientResponseException.class})
-    public ResponseEntity<String> handleRestClientResponseException(RestClientResponseException e) {
-        secureLogger.error("RestClientResponseException : ", e);
-        logger.error("RestClientResponseException : {}", ExceptionUtils.getStackTrace(e));
-        return ResponseEntity.status(e.getRawStatusCode()).header("message", e.getMessage()).build();
+    public Map<String, String> handleRestClientResponseException(RestClientResponseException e) {
+        secureLogger.error("RestClientResponseException : {} {}", e.getResponseBodyAsString(), e);
+        logger.error("RestClientResponseException : {} {} {}", e.getRawStatusCode(), e.getStatusText(), ExceptionUtils.getStackTrace(e));
+        return Map.of("error", "Feil mot ekstern tjeneste " + e.getRawStatusCode() +  " " + e.getResponseBodyAsString() + " Message: " + e.getMessage());
     }
 
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<String> handleException(Exception e) {
+    public Map<String, String> handleException(Exception e) {
         secureLogger.error("Exception : ", e);
         logger.error("Exception : {}", ExceptionUtils.getStackTrace(e));
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body("Det oppstod en Feil. Feilen er logget og vi ser på problemet!");
+        return Map.of("error", "Det oppstod en feil " + e.getMessage());
     }
 
 }
