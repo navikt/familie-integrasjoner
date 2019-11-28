9a40f59b7ab5913a839efaa39f6dad1b271c6039
Nina Kjekstad <ninakjeks@gmail.com>
2019-11-28 13:36:01 +0100

    trenger ikke beskytte kodeverk

--- src/main/java/no/nav/familie/integrasjoner/kodeverk/KodeverkController.kt
+++ src/main/java/no/nav/familie/integrasjoner/kodeverk/KodeverkController.kt
@@ -1,11 +1,11 @@
 package no.nav.familie.integrasjoner.kodeverk
 
-import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@ProtectedWithClaims(issuer = "azuread")
+@Unprotected
 @RequestMapping(path = ["/api/kodeverk/poststed"], produces = [MediaType.TEXT_PLAIN_VALUE])
 class PostnummerController(val kodeverkService: KodeverkService) {
 
