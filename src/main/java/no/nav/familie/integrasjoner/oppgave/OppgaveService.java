ae0e9bcd84113ae6c5caa9b8c3d30db4391320f3
Stig Helge Strøm <stig.helge.strom@nav.no>
2019-12-02 10:54:00 +0100

    Fjernet imports

--- src/main/java/no/nav/familie/integrasjoner/oppgave/OppgaveService.java
+++ src/main/java/no/nav/familie/integrasjoner/oppgave/OppgaveService.java
@@ -7,7 +7,6 @@ import no.nav.sbl.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 
