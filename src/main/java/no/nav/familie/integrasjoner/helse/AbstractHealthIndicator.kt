8ef35ba253e067b19b0dd602d11f42b5dd286b88
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-01-08 12:51:16 +0100

    Fiks url og logger

--- src/main/java/no/nav/familie/integrasjoner/helse/AbstractHealthIndicator.kt
+++ src/main/java/no/nav/familie/integrasjoner/helse/AbstractHealthIndicator.kt
@@ -2,12 +2,15 @@ package no.nav.familie.integrasjoner.helse
 
 import io.micrometer.core.instrument.Counter
 import no.nav.familie.integrasjoner.client.Pingable
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.core.NestedExceptionUtils
 
 internal abstract class AbstractHealthIndicator(private val pingable: Pingable) : HealthIndicator {
 
+    protected val log: Logger = LoggerFactory.getLogger(this::class.java)
     protected abstract val failureCounter: Counter
 
     override fun health(): Health {
@@ -16,6 +19,7 @@ internal abstract class AbstractHealthIndicator(private val pingable: Pingable) 
             Health.up().build()
         } catch (e: Exception) {
             failureCounter.increment()
+            log.warn("Feil ved helsesjekk", e)
             Health.status("DOWN-NONCRITICAL")
                     .withDetail("Feilmelding", NestedExceptionUtils.getMostSpecificCause(e).javaClass.name + ": " + e.message)
                     .build()
