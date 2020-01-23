806eb9bf4882a8ee68e6800c88ea722dc5213ede
Nina Kjekstad <ninakjek@users.noreply.github.com>
2020-01-23 14:35:49 +0100

    Update KodeverkController.kt

--- src/main/java/no/nav/familie/integrasjoner/kodeverk/KodeverkController.kt
+++ src/main/java/no/nav/familie/integrasjoner/kodeverk/KodeverkController.kt
@@ -1,7 +1,7 @@
 package no.nav.familie.integrasjoner.kodeverk
 
 import no.nav.familie.kontrakter.felles.Ressurs
-import no.nav.security.token.support.core.api.Unprotected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
