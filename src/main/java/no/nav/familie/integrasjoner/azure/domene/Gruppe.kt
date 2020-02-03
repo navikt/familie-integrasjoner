30c75ad236d7882a82e9ecea8d82e85cb6d618d8
Bent Lorentzen <bent.lorentzen@nav.no>
2020-02-03 11:35:40 +0100

    Mangelfulle kotlin-konvertering.

--- src/main/java/no/nav/familie/integrasjoner/azure/domene/Gruppe.kt
+++ src/main/java/no/nav/familie/integrasjoner/azure/domene/Gruppe.kt
@@ -1,8 +1,5 @@
 package no.nav.familie.integrasjoner.azure.domene
 
-class Gruppe {
-    var id: String? = null
-    var onPremisesSamAccountName: String? = null
-    var displayName: String? = null
-
-}
+class Gruppe(val id: String?,
+             val onPremisesSamAccountName: String?,
+             val displayName: String?)
