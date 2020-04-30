931e5081e582f40c76eeb09cbf8b319ce8f9b7fe
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-04-30 12:40:25 +0200

    Lagt til kategori på vedlegg metadata

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVedtakVedleggMetadata.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVedtakVedleggMetadata.kt
@@ -7,13 +7,13 @@ import org.springframework.stereotype.Component
 object BarnetrygdVedleggMetadata : DokumentMetadata {
 
     override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
-    override val fagsakSystem: String? = null
+    override val fagsakSystem: String? = "BA"
     override val tema: String = "BAR"
     override val behandlingstema: String? = null
     override val kanal: String? = null
     override val dokumentTypeId: String = "BARNETRYGD_VEDLEGG"
     override val tittel: String? = null
     override val brevkode: String? = null
-    override val dokumentKategori: String = "????"
+    override val dokumentKategori: String = "VB"
 
 }
