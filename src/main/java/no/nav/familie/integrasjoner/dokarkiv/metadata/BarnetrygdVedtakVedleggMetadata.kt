446a0e09ad312964597e66051103675e206012de
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-30 10:32:14 +0200

    Generell metadata for barnetrygd-vedlegg

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVedtakVedleggMetadata.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVedtakVedleggMetadata.kt
@@ -4,14 +4,14 @@ import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
 import org.springframework.stereotype.Component
 
 @Component
-object BarnetrygdVedtakVedleggMetadata : DokumentMetadata {
+object BarnetrygdVedleggMetadata : DokumentMetadata {
 
     override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
     override val fagsakSystem: String? = null
     override val tema: String = "BAR"
     override val behandlingstema: String? = null
     override val kanal: String? = null
-    override val dokumentTypeId: String = "BARNETRYGD_VEDTAK_VEDLEGG"
+    override val dokumentTypeId: String = "BARNETRYGD_VEDLEGG"
     override val tittel: String? = null
     override val brevkode: String? = null
     override val dokumentKategori: String = "????"
