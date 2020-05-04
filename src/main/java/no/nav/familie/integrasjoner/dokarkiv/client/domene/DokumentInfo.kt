55cd6565affd695160adad7ba6816d167c52b307
tnarland <47184872+tnarland@users.noreply.github.com>
2020-05-04 22:56:24 +0200

    Fjerner felt dokumentKategori

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/DokumentInfo.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/DokumentInfo.kt
@@ -2,5 +2,4 @@ package no.nav.familie.integrasjoner.dokarkiv.client.domene
 
 class DokumentInfo(val dokumentInfoId: String? = null,
                    val tittel: String? = null,
-                   val brevkode: String? = null,
-                   val dokumentKategori: String? = null)
+                   val brevkode: String? = null)
