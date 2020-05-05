e49ba8260706eca72456ecf0d577e6ab0b7553e0
tnarland <47184872+tnarland@users.noreply.github.com>
2020-05-05 09:20:30 +0200

    Utvidet OppdaterJournalpostRequest

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/DokumentInfo.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/DokumentInfo.kt
@@ -1,3 +1,5 @@
 package no.nav.familie.integrasjoner.dokarkiv.client.domene
 
-class DokumentInfo(val dokumentInfoId: String? = null)
+class DokumentInfo(val dokumentInfoId: String? = null,
+                   val tittel: String? = null,
+                   val brevkode: String? = null)
