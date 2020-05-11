797c8f2aa71085d1a82f87eef847db85940a8bb3
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-05-11 17:51:31 +0200

    Swagger sliter med objekter med samme navn, så renamet Bruker brukt i Dokarkiv til DokarkivBruker, eller så vil den bruke Bruker i saf

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/OppdaterJournalpostRequest.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/OppdaterJournalpostRequest.kt
@@ -1,11 +1,10 @@
 package no.nav.familie.integrasjoner.dokarkiv.client.domene
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import java.time.LocalDate
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class OppdaterJournalpostRequest(val avsenderMottaker: AvsenderMottaker? = null,
-                                      val bruker: Bruker? = null,
+                                      val bruker: DokarkivBruker? = null,
                                       val tema: String? = null,
                                       val behandlingstema: String? = null,
                                       val tittel: String? = null,
