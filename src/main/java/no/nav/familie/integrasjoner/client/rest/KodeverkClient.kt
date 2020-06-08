ba91ca169b47a5e0926e88f49b7379e6cb70667e
Johan Blomgren <johan.blomgren@nav.no>
2020-06-08 16:28:14 +0200

    Sender med token til kodeverk

--- src/main/java/no/nav/familie/integrasjoner/client/rest/KodeverkClient.kt
+++ src/main/java/no/nav/familie/integrasjoner/client/rest/KodeverkClient.kt
@@ -1,17 +1,21 @@
 package no.nav.familie.integrasjoner.client.rest
 
 import no.nav.familie.http.client.AbstractPingableRestClient
+import no.nav.familie.http.sts.StsRestClient
 import no.nav.familie.http.util.UriUtil
 import no.nav.familie.integrasjoner.kodeverk.domene.KodeverkDto
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
 import java.net.URI
 
 @Component
 class KodeverkClient(@Value("\${KODEVERK_URL}") private val kodeverkUri: URI,
-                     @Qualifier("sts") private val restTemplate: RestOperations)
+                     @Qualifier("sts") val restTemplate: RestOperations,
+                     private val stsRestClient: StsRestClient)
     : AbstractPingableRestClient(restTemplate, "kodeverk") {
 
     override val pingUri: URI = UriUtil.uri(kodeverkUri, PATH_PING)
@@ -29,13 +33,21 @@ class KodeverkClient(@Value("\${KODEVERK_URL}") private val kodeverkUri: URI,
     }
 
     fun hentLandkoderMedHistorikk(): KodeverkDto {
-        return getForEntity(UriUtil.uri(kodeverkUri, "Landkoder", QUERY_MED_HISTORIKK))
+        return getForEntity(UriUtil.uri(kodeverkUri, "Landkoder", QUERY_MED_HISTORIKK), httpHeaders())
     }
 
     private fun kodeverkUri(kodeverksnavn: String,
                             medHistorikk: Boolean = false): URI {
         val query = if (medHistorikk) QUERY_MED_HISTORIKK else QUERY
         return UriUtil.uri(kodeverkUri, "api/v1/kodeverk/$kodeverksnavn/koder/betydninger", query)
+    }
+
+    private fun httpHeaders(): HttpHeaders {
+        return HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_JSON
+            accept = listOf(MediaType.APPLICATION_JSON)
+            add("Nav-Consumer-Token", "Bearer ${stsRestClient.systemOIDCToken}")
+        }
     }
 
     companion object {
