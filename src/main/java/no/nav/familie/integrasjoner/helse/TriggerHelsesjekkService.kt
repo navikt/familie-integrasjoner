e368bc07762a888d5469b1daece0fd3507961fc3
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-06-16 20:44:18 +0200

    Service for helsesjekker

---
+++ src/main/java/no/nav/familie/integrasjoner/helse/TriggerHelsesjekkService.kt
@@ -0,0 +1,26 @@
+package no.nav.familie.integrasjoner.helse
+
+import no.nav.familie.http.health.AbstractHealthIndicator
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+
+@Service
+class TriggerHelsesjekkService {
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @Autowired
+    lateinit var helsesjekkerEksterneSystemer: List<AbstractHealthIndicator>
+
+    @Scheduled(fixedDelay = 30000, initialDelay = 30000)
+    fun triggerPing() {
+        log.debug("Kjører helsesjekker")
+        helsesjekkerEksterneSystemer.forEach {
+            it.health()
+        }
+    }
+
+}
