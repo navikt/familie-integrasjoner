0817901ef0cbacb6dc51223cbcc3d81ab61c2323
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-07-21 09:57:07 +0200

    Legg til metadata for ulike varianter av søknad for barnetrygd

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdSøknadOrdinær.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdSøknadOrdinær : DokumentMetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
+    override val fagsakSystem: String? = null
+    override val tema: String = "BAR"
+    override val behandlingstema: String? = "ab0180" // https://confluence.adeo.no/display/BOA/Behandlingstema
+    override val kanal: String? = "NAV_NO"
+    override val dokumentTypeId: String = "BARNETRYGD_ORDINÆR"
+    override val tittel: String? = "Søknad om barnetrygd ordinær"
+    override val brevkode: String? = "NAV 33-00.07"
+    override val dokumentKategori: String = "SOK"
+
+}
---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdSøknadUtvidet.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdSøknadUtvidet : DokumentMetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
+    override val fagsakSystem: String? = null
+    override val tema: String = "BAR"
+    override val behandlingstema: String? = "ab0096" // https://confluence.adeo.no/display/BOA/Behandlingstema
+    override val kanal: String? = "NAV_NO"
+    override val dokumentTypeId: String = "BARNETRYGD_UTVIDET"
+    override val tittel: String? = "Søknad om utvidet barnetrygd"
+    override val brevkode: String? = "NAV 33-00.09"
+    override val dokumentKategori: String = "SOK"
+
+}
