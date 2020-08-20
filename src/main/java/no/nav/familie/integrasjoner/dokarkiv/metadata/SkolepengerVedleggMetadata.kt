5b229db115cc141c96ab051e5c920ea3979d8e81
ma10s <mattis.janitz@nav.no>
2020-08-20 12:37:58 +0200

    Legg til metadata for skolepenger

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/SkolepengerVedleggMetadata.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import org.springframework.stereotype.Component
+
+@Component
+object SkolepengerVedleggMetadata : DokumentMetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
+    override val fagsakSystem: String? = null
+    override val tema: String = "ENF"
+    override val behandlingstema: String? = null // https://confluence.adeo.no/display/BOA/Behandlingstema
+    override val kanal: String? = null
+    override val dokumentTypeId: String = "SKOLEPENGER_VEDLEGG"
+    override val tittel: String? = null
+    override val brevkode: String? = null
+    override val dokumentKategori: String = "IS"
+
+}
