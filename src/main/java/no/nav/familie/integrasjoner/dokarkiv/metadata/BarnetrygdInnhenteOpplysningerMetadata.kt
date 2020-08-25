75cc049930ce2f73191f1842baafb95691cd8e87
Nina Kjekstad <nina.kjekstad@nav.no>
2020-08-25 17:24:57 +0200

    Legg til metadata for manuelt brev Innhente opplysninger

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInnhenteOpplysningerMetadata.kt
@@ -0,0 +1,20 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdInnhenteOpplysningerMetadata : DokumentMetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: String? = "BA"
+    override val tema: String = "BAR"
+    override val behandlingstema: String? = null
+    override val kanal: String? = null
+    override val dokumentTypeId: String = "BARNETRYGD_INNHENTE_OPPLYSNINGER"
+    override val tittel: String? = null
+    override val brevkode: String? = null
+    override val dokumentKategori: String = "VB"
+
+
+}
