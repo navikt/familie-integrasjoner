ac77bf1439e95e6b4d1195e3c0edbd8f4e1e9439
Nina Kjekstad <nina.kjekstad@nav.no>
2020-09-17 13:21:41 +0200

    Bugfix

--- src/test/java/no/nav/familie/integrasjoner/oppgave/OppgaveRestClientTestConfig.kt
+++ src/test/java/no/nav/familie/integrasjoner/oppgave/OppgaveRestClientTestConfig.kt
@@ -3,11 +3,12 @@ package no.nav.familie.integrasjoner.oppgave
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.integrasjoner.client.rest.OppgaveRestClient
-import no.nav.familie.kontrakter.felles.oppgave.Oppgave
+import no.nav.familie.kontrakter.felles.oppgave.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
+import java.time.LocalDate
 
 @Configuration
 class OppgaveRestClientTestConfig {
@@ -16,8 +17,32 @@ class OppgaveRestClientTestConfig {
     @Profile("mock-oppgave")
     @Primary
     fun OppgaveMockRestClient(): OppgaveRestClient {
-        val klient: OppgaveRestClient = mockk(relaxed = true)
-        val response = Oppgave(id = 42)
+        val klient: OppgaveRestClient = mockk(relaxed = false)
+        val response = Oppgave(id = 42,
+                               aktoerId = "1234",
+                               identer = listOf(OppgaveIdentV2("11111111111", IdentGruppe.FOLKEREGISTERIDENT)),
+                               journalpostId = "1234",
+                               tildeltEnhetsnr = "4820",
+                               tilordnetRessurs = "test@nav.no",
+                               behandlesAvApplikasjon = "FS22",
+                               beskrivelse = "Beskrivelse for oppgave",
+                               tema = Tema.BAR,
+                               oppgavetype = Oppgavetype.Journalføring.value,
+                               opprettetTidspunkt = LocalDate.of(
+                                       2020,
+                                       1,
+                                       1).toString(),
+                               fristFerdigstillelse = LocalDate.of(
+                                       2020,
+                                       2,
+                                       1).toString(),
+                               prioritet = OppgavePrioritet.NORM,
+                               status = StatusEnum.OPPRETTET
+        )
+
+        every {
+            klient.finnOppgaver(any())
+        } returns FinnOppgaveResponseDto(1, listOf(response))
 
         every {
             klient.finnOppgave(any())
