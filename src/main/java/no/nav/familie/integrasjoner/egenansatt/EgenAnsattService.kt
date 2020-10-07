c5ced7a0892dfd82045a66eebd0c70e4d5e53e8e
Marcus Gustafson <marcus.gustafson@computas.com>
2020-10-07 16:19:03 +0200

    Temp logging for å verifisere at endringen virker

--- src/main/java/no/nav/familie/integrasjoner/egenansatt/EgenAnsattService.kt
+++ src/main/java/no/nav/familie/integrasjoner/egenansatt/EgenAnsattService.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.integrasjoner.egenansatt
 
 import no.nav.familie.integrasjoner.client.rest.EgenAnsattRestClient
+import no.nav.familie.integrasjoner.dokarkiv.DokarkivController
+import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
@@ -9,6 +11,12 @@ class EgenAnsattService(private val egenAnsattRestClient: EgenAnsattRestClient) 
 
     @Cacheable("erEgenAnsatt")
     fun erEgenAnsatt(fnr: String): Boolean {
+        LOG.info("Er egen ansatt")
+        val erEgenAnsatt = egenAnsattRestClient.erEgenAnsatt(fnr)
+        LOG.info("Er egenansatt: $erEgenAnsatt")
         return egenAnsattRestClient.erEgenAnsatt(fnr)
+    }
+    companion object {
+        private val LOG = LoggerFactory.getLogger(EgenAnsattService::class.java)
     }
 }
