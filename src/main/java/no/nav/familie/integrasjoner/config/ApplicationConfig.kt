35e53456f5eb4e6c405d23b16cc31fcee427b6d6
Johan Blomgren <johan.blomgren@nav.no>
2020-11-06 15:09:46 +0100

    Oppgradert swagger til 3.0.0

--- src/main/java/no/nav/familie/integrasjoner/config/ApplicationConfig.kt
+++ src/main/java/no/nav/familie/integrasjoner/config/ApplicationConfig.kt
@@ -1,30 +1,21 @@
 package no.nav.familie.integrasjoner.config
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import no.nav.familie.http.interceptor.BearerTokenClientInterceptor
-import no.nav.familie.http.interceptor.ConsumerIdClientInterceptor
-import no.nav.familie.http.interceptor.MdcValuesPropagatingClientInterceptor
-import no.nav.familie.http.interceptor.StsBearerTokenClientInterceptor
 import no.nav.familie.log.filter.LogFilter
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Import
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.client.RestOperations
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootConfiguration
 @ComponentScan("no.nav.familie.integrasjoner")
 @ConfigurationPropertiesScan
-@EnableSwagger2
-@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
+@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger"])
 @EnableOAuth2Client(cacheEnabled = true)
 @EnableScheduling
 class ApplicationConfig {
