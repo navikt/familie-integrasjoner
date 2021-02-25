0b89da6fba6cfd36b5a139cb9ae4f4a805ffc960
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2021-02-25 21:45:42 +0100

    OvergangsstønadBlankettMetadata lagt til som egen DokumentMetadata (#320)
    * OvergangsstønadBlankettMetadata lagt til som egen DokumentMetadata
    
    * Rettinger etter QA

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/OvergangsstønadBlankettMetadata.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import org.springframework.stereotype.Component
+
+@Component
+object OvergangsstønadBlankettMetadata : DokumentMetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.NOTAT
+    override val fagsakSystem: String = "Infotrygd"
+    override val tema: String = "ENF"
+    override val behandlingstema: String = "ab0071"
+    override val kanal: String? = null
+    override val dokumentTypeId: String = "OVERGANGSSTØNAD_BLANKETT"
+    override val tittel: String = "Blankett for overgangsstønad - enslig mor eller far"
+    override val brevkode: String? = null
+    override val dokumentKategori: String = "FORVALTNINGSNOTAT"
+
+}
