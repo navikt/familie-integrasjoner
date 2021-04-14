a5979d198c98463a1fd0f01528bfa3f636b937d9
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-04-14 14:35:21 +0200

    organisasjonsklient

---
+++ src/main/java/no/nav/familie/integrasjoner/organisasjon/OrganisasjonController.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.integrasjoner.organisasjon
+
+import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.Ressurs.Companion.success
+import no.nav.familie.kontrakter.felles.organisasjon.Organisasjon
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@ProtectedWithClaims(issuer = "azuread")
+@RequestMapping("/api/organisasjon")
+class OrganisasjonController(private val organisasjonService: OrganisasjonService) {
+
+    @GetMapping(path = ["/{orgnr}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun erOrganisasjon(@PathVariable orgnr: String): Ressurs<Organisasjon> {
+        return success(data = organisasjonService.hentOrganisasjon(orgnr))
+    }
+
+}
---
+++ src/test/java/no/nav/familie/integrasjoner/organisasjon/OrganisasjonTestConfig.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.integrasjoner.organisasjon
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.integrasjoner.client.soap.OrganisasjonSoapClient
+import no.nav.tjeneste.virksomhet.organisasjon.v5.informasjon.Organisasjon
+import no.nav.tjeneste.virksomhet.organisasjon.v5.informasjon.UstrukturertNavn
+import no.nav.tjeneste.virksomhet.organisasjon.v5.meldinger.HentOrganisasjonResponse
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+
+@Configuration
+class OrganisasjonTestConfig {
+
+    @Bean
+    @Profile("mock-organisasjon")
+    @Primary
+    fun organisasjonSoapClientMock(): OrganisasjonSoapClient {
+        val organisasjonSoapClientMock: OrganisasjonSoapClient = mockk(relaxed = true)
+        val organisasjon = HentOrganisasjonResponse().apply {
+            organisasjon = Organisasjon().apply { navn = UstrukturertNavn() }
+        }
+        every { organisasjonSoapClientMock.hentOrganisasjon(any()) }
+                .returns(organisasjon)
+        return organisasjonSoapClientMock
+    }
+}
