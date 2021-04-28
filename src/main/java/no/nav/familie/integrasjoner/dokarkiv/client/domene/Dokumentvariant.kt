d436525764eb69a18dab7dbac66a4ce87955203d
Bent J. Lorentzen <bent.lorentzen@nav.no>
2021-04-28 14:58:53 +0200

    Prøver å få git til å skjønne at filer er renamet

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/client/domene/Dokumentvariant.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.familie.integrasjoner.dokarkiv.client.domene
-
-class Dokumentvariant(val filtype: String,
-                      val variantformat: String,
-                      val fysiskDokument: ByteArray,
-                      val filnavn: String?)
