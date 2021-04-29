1cfe7d1a56a830eb2949294d61abfef30ed63c7d
Øyvind Hagen <oyvind.hagen@nav.no>
2021-04-29 15:51:28 +0200

    Bumpet kontrakter og lagt til nye metadata for ulike vedtakstyper i BA.

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVedtakInnvilgetMetadata.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdVedtakInnvilgetMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema = Behandlingstema.Barnetrygd
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_VEDTAK_INNVILGELSE
+    override val tittel: String = "Vedtak om innvilgelse av barnetrygd"
+    override val brevkode: String = "BAA1"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.VB
+
+}
