397a244ce0d92ea89f6b1ef70a5dcda2cf0258c3
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2021-06-07 15:56:59 +0200

    Oppdatert til å takle oppslag på navIdent (#403)
    * Oppdatert til å takle oppslag på navIdent

--- src/main/java/no/nav/familie/integrasjoner/azure/domene/AzureAdBruker.kt
+++ src/main/java/no/nav/familie/integrasjoner/azure/domene/AzureAdBruker.kt
@@ -1,7 +1,10 @@
 package no.nav.familie.integrasjoner.azure.domene
 
+import java.util.UUID
 
-data class AzureAdBruker(val id: String,
+
+data class AzureAdBruker(val id: UUID,
+                         val onPremisesSamAccountName: String,
                          val userPrincipalName: String,
                          val givenName: String,
                          val surname: String)
--- src/main/java/no/nav/familie/integrasjoner/client/rest/AzureGraphRestClient.kt
+++ src/main/java/no/nav/familie/integrasjoner/client/rest/AzureGraphRestClient.kt
@@ -1,10 +1,12 @@
 package no.nav.familie.integrasjoner.client.rest
 
 import no.nav.familie.http.client.AbstractRestClient
-import no.nav.familie.integrasjoner.azure.domene.Grupper
 import no.nav.familie.integrasjoner.azure.domene.AzureAdBruker
+import no.nav.familie.integrasjoner.azure.domene.AzureAdBrukere
+import no.nav.familie.integrasjoner.azure.domene.Grupper
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
 import org.springframework.web.util.UriComponentsBuilder
@@ -19,8 +21,22 @@ class AzureGraphRestClient(@Qualifier("jwtBearer") restTemplate: RestOperations,
 
     fun saksbehandlerUri(id: String): URI = UriComponentsBuilder.fromUri(aadGraphURI).pathSegment(USERS, id).build().toUri()
 
+    fun saksbehandlersøkUri(navIdent: String): URI =
+            UriComponentsBuilder.fromUri(aadGraphURI)
+                    .pathSegment(USERS)
+                    .queryParam("\$search", "\"onPremisesSamAccountName:{navIdent}\"")
+                    .queryParam("\$select", "givenName,surname,onPremisesSamAccountName,id,userPrincipalName")
+                    .buildAndExpand(navIdent)
+                    .toUri()
+
     val grupperUri: URI = UriComponentsBuilder.fromUri(aadGraphURI).pathSegment(ME, GRUPPER).build().toUri()
 
+
+    fun finnSaksbehandler(navIdent: String): AzureAdBrukere {
+        return getForEntity(saksbehandlersøkUri(navIdent), HttpHeaders().apply {
+            add("ConsistencyLevel", "eventual")
+        })
+    }
 
     fun hentSaksbehandler(): AzureAdBruker {
         return getForEntity(saksbehandlerUri)
@@ -38,5 +54,6 @@ class AzureGraphRestClient(@Qualifier("jwtBearer") restTemplate: RestOperations,
         private const val ME = "me"
         private const val USERS = "users"
         private const val GRUPPER = "memberOf"
+
     }
 }
