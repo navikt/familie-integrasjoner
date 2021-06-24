d9bed3a25e65f97be1404cd66ff0faf127874f17
Henning Solberg <henning.solberg@nav.no>
2021-06-24 14:07:00 +0200

    Wlapper saksbehandler i Ressurs

--- src/main/java/no/nav/familie/integrasjoner/saksbehandler/SaksbehandlerController.kt
+++ src/main/java/no/nav/familie/integrasjoner/saksbehandler/SaksbehandlerController.kt
@@ -1,9 +1,14 @@
 package no.nav.familie.integrasjoner.saksbehandler
 
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.context.annotation.Profile
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = ["/api/saksbehandler"])
@@ -12,8 +17,8 @@ class SaksbehandlerController(private val saksbehandlerService: SaksbehandlerSer
 
     @GetMapping(path = ["/{id}"])
     @ProtectedWithClaims(issuer = "azuread")
-    fun hentSaksbehandler(@PathVariable id: String): Saksbehandler { // id kan være azure-id, e-post eller nav-ident
-        return saksbehandlerService.hentSaksbehandler(id)
+    fun hentSaksbehandler(@PathVariable id: String): ResponseEntity<Ressurs<Saksbehandler>> { // id kan være azure-id, e-post eller nav-ident
+        return ResponseEntity.ok().body(Ressurs.success(saksbehandlerService.hentSaksbehandler(id), "Hent saksbehandler OK"))
     }
 
 }
