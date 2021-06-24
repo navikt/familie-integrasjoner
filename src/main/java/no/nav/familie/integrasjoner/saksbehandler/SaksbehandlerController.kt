39718912f97ecb3f2addb4fef96155704cde7daf
Henning Solberg <henning.solberg@nav.no>
2021-06-24 19:41:19 +0200

    Fjerner ResponseEntity

--- src/main/java/no/nav/familie/integrasjoner/saksbehandler/SaksbehandlerController.kt
+++ src/main/java/no/nav/familie/integrasjoner/saksbehandler/SaksbehandlerController.kt
@@ -4,7 +4,6 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.context.annotation.Profile
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,8 +16,8 @@ class SaksbehandlerController(private val saksbehandlerService: SaksbehandlerSer
 
     @GetMapping(path = ["/{id}"])
     @ProtectedWithClaims(issuer = "azuread")
-    fun hentSaksbehandler(@PathVariable id: String): ResponseEntity<Ressurs<Saksbehandler>> { // id kan være azure-id, e-post eller nav-ident
-        return ResponseEntity.ok().body(Ressurs.success(saksbehandlerService.hentSaksbehandler(id), "Hent saksbehandler OK"))
+    fun hentSaksbehandler(@PathVariable id: String): Ressurs<Saksbehandler> { // id kan være azure-id, e-post eller nav-ident
+        return Ressurs.success(saksbehandlerService.hentSaksbehandler(id), "Hent saksbehandler OK")
     }
 
 }
