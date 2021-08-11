db1db71f83581d210b8aa97149552257a3efe69a
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-08-11 10:19:45 +0200

    Skal ha egne dokumentmetadata-objekter for ettersending i EF

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/SkolepengerEttersendingMetadata.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object SkolepengerEttersendingMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
+    override val fagsakSystem: Fagsystem? = null
+    override val tema: Tema = Tema.ENF
+    override val behandlingstema: Behandlingstema = Behandlingstema.Skolepenger
+    override val kanal: String = "NAV_NO"
+    override val dokumenttype: Dokumenttype = Dokumenttype.SKOLEPENGER_ETTERSENDING
+    override val tittel: String = "Ettersending skolepenger - enslig mor eller far"
+    override val brevkode: String = "NAVe 15-00.04"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.IS
+
+}
