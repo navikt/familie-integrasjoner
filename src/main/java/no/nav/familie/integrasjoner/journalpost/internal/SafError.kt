944bc5a19831f1af6e1ed61b075110294a083e10
Henning Håkonsen <henning.hakonsen@nav.no>
2021-08-24 09:29:06 +0200

    Bruker code på extension til å bedømme om det er forbidden istedet for tekst.

--- src/main/java/no/nav/familie/integrasjoner/journalpost/internal/SafError.kt
+++ src/main/java/no/nav/familie/integrasjoner/journalpost/internal/SafError.kt
@@ -2,5 +2,13 @@ package no.nav.familie.integrasjoner.journalpost.internal
 
 data class SafError(
         val message: String? = null,
+        val code: SafErrorCode? = null,
         val exceptionType: String? = null,
         val exception: String? = null)
+
+enum class SafErrorCode {
+    forbidden,
+    not_found,
+    bad_request,
+    server_error
+}
