1bcbb8b5232112b474bd6c414647ffb1e8321b72
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-08-30 16:49:17 +0200

    Mer fleksibel oppgavetildeling + e2e-mock (#454)
    * Revert "Revert "Mer fleksibel oppgavetildeling (#448)" (#450)"
    
    This reverts commit 34001ba0467cfd3291a029a3ef04d87d75740bd4.
    
    * Revert "Fjern tag (#453)"
    
    This reverts commit c704271223b42157a0a8e7e386b05422cacb7c2e.
    
    * Revert "Legg til e2e-run for ba-sak (#452)"
    
    This reverts commit 60104759f5433c27a1c104305e48d030850a40b4.
    
    * Midlertidig håndtering av e2e

--- src/main/java/no/nav/familie/integrasjoner/saksbehandler/SaksbehandlerService.kt
+++ src/main/java/no/nav/familie/integrasjoner/saksbehandler/SaksbehandlerService.kt
@@ -2,15 +2,25 @@ package no.nav.familie.integrasjoner.saksbehandler
 
 import no.nav.familie.integrasjoner.client.rest.AzureGraphRestClient
 import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
-class SaksbehandlerService(private val azureGraphRestClient: AzureGraphRestClient) {
+class SaksbehandlerService(private val azureGraphRestClient: AzureGraphRestClient,
+                           private val environment: Environment) {
 
     private val lengdeNavIdent = 7
 
     fun hentSaksbehandler(id: String): Saksbehandler {
+
+        // TODO: Midlertidig for å få ut funksjonalitet. Fjern når ba-sak-e2e fases ut.
+        if (environment.activeProfiles.any { it == "e2e" }) {
+            return Saksbehandler(azureId = UUID.randomUUID(),
+                                 navIdent = id,
+                                 fornavn = "Mocka",
+                                 etternavn = "Saksbehandler")
+        }
 
         if (id == ID_VEDTAKSLØSNINGEN) {
             return Saksbehandler(UUID.randomUUID(),
@@ -37,8 +47,12 @@ class SaksbehandlerService(private val azureGraphRestClient: AzureGraphRestClien
                              azureAdBruker.surname)
     }
 
+    fun hentNavIdent(saksbehandlerId: String): String =
+            saksbehandlerId.takeIf { it.length == lengdeNavIdent } ?: hentSaksbehandler(saksbehandlerId).navIdent
+
     companion object {
 
         const val ID_VEDTAKSLØSNINGEN = "VL"
     }
+
 }
