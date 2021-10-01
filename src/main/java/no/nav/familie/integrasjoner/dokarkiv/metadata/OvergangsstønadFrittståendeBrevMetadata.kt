13d1d52179fc08c9a87919f2651eecde1eca1f83
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2021-10-01 13:49:40 +0200

    Lagt til metadata-klasse for ny dokumenttype: OvergangsstønadFrittstå… (#465)
    * Lagt til metadata-klasse for ny dokumenttype: OvergangsstønadFrittståendeBrevMetadata
    
    * Rettet default tittel på frittstående brev

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/OvergangsstønadFrittståendeBrevMetadata.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object OvergangsstønadFrittståendeBrevMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.EF
+    override val tema: Tema = Tema.ENF
+    override val behandlingstema: Behandlingstema = Behandlingstema.Overgangsstønad
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.OVERGANGSSTØNAD_FRITTSTÅENDE_BREV
+    override val tittel: String = "Brev om overgangsstønad"
+    override val brevkode: String = "EFA1"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
+
