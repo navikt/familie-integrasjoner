b7d2290783d6e264eaa14b93fc9df2492adae226
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-11-09 14:09:02 +0100

    Skal kunne journalføre blankett mot EF som fagsak ved normal saksbehandlingsflyt i ny løsning

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/OvergangsstønadBlankettSaksbehandlingMetadata.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object OvergangsstønadBlankettSaksbehandlingMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.NOTAT
+    override val fagsakSystem: Fagsystem = Fagsystem.EF
+    override val tema: Tema = Tema.ENF
+    override val behandlingstema: Behandlingstema = Behandlingstema.Overgangsstønad
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.OVERGANGSSTØNAD_BLANKETT_SAKSBEHANDLING
+    override val tittel: String = "Blankett for overgangsstønad - enslig mor eller far"
+    override val brevkode: String? = null
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.FORVALTNINGSNOTAT
+
+}
