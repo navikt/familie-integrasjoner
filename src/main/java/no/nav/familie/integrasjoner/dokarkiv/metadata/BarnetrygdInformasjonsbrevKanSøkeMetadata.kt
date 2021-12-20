9aadc22167cec7cca62636916196c3979dbf766f
halvormundal <halvorbmundal@gmail.com>
2021-12-20 14:55:01 +0100

    Legg til nye brev og oppdater familie-kontrakter

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInformasjonsbrevKanSøkeMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdInformasjonsbrevKanSøkeMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_INFORMASJONSBREV_KAN_SØKE
+    override val tittel: String = "Informasjonsbrev kan søke"
+    override val brevkode: String = "informasjonsbrevKanSøke"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
