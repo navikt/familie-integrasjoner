60fa2ca7788fb2d6cc3e7aa00a99821362e1e3e6
halvorbmundal <halvorbmundal@users.noreply.github.com>
2021-12-20 15:03:39 +0100

    Legg til nye brev (#525)
    * Legg til nye brev og oppdater familie-kontrakter
    
    * Endre til bindestrek-case og fjern æøå

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInformasjonsbrevFødselMindreårigMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdInformasjonsbrevFødselMindreårigMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_INFORMASJONSBREV_FØDSEL_MINDREÅRIG
+    override val tittel: String = "Informasjonsbrev fødsel mindreårig"
+    override val brevkode: String = "informasjonsbrev-fodsel-mindreaarig"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
