fcd1adacc4ba1b8d53b74c9aa2b64c7824858384
halvormundal <halvorbmundal@gmail.com>
2022-01-05 13:05:35 +0100

    Fjern exception handler

--- src/main/java/no/nav/familie/integrasjoner/infotrygd/InfotrygdController.kt
+++ src/main/java/no/nav/familie/integrasjoner/infotrygd/InfotrygdController.kt
@@ -13,9 +13,13 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.HttpClientErrorException
-import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestClientResponseException
 
 @RestController
