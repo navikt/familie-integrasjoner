a61e912e50c23e349db7d2453e7d25d7f9831a0a
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-02-09 13:39:54 +0100

    Legger til vedtaksbrevmetadata for barnetilsyn og skolepenger

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetilsynVedtaksbrevMetadata.kt
@@ -0,0 +1,23 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetilsynVedtaksbrevMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.EF
+    override val tema: Tema = Tema.ENF
+    override val behandlingstema: Behandlingstema = Behandlingstema.Barnetilsyn
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.VEDTAKSBREV_BARNETILSYN
+    override val tittel: String? = null
+    override val brevkode: String = "EFA1"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.VB
+}
+
