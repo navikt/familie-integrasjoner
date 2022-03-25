464b9317d450ff4c330ca2c6e10603244838bde5
halvormundal <halvorbmundal@gmail.com>
2022-03-25 11:02:12 +0100

    Bruk securelogger og ikke vanglig logger

--- src/main/java/no/nav/familie/integrasjoner/dokdist/DokdistController.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokdist/DokdistController.kt
@@ -7,7 +7,12 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.HttpClientErrorException
 import javax.validation.Valid
 
@@ -18,22 +23,19 @@ class DokdistController(private val dokdistService: DokdistService) {
 
     @ExceptionHandler(HttpClientErrorException::class)
     fun handleHttpClientException(e: HttpClientErrorException): ResponseEntity<Ressurs<Any>> {
-        LOG.warn("Feil ved distribusjon: ${e.message}")
+        secureLogger.warn("Feil ved distribusjon: ${e.message}")
         return ResponseEntity.status(e.rawStatusCode).body(Ressurs.failure(e.message, error = e))
     }
 
-
     @PostMapping("v1")
     @ResponseStatus(HttpStatus.OK)
-    fun distribuerJournalpost(@RequestBody request: @Valid DistribuerJournalpostRequest)
-            : ResponseEntity<Ressurs<String>>  {
+    fun distribuerJournalpost(@RequestBody request: @Valid DistribuerJournalpostRequest): ResponseEntity<Ressurs<String>> {
 
         val response = dokdistService.distribuerDokumentForJournalpost(request)
         return ResponseEntity.ok(success(response?.bestillingsId ?: throw NullResponseException()))
     }
 
     companion object {
-        private val LOG = LoggerFactory.getLogger(DokdistController::class.java)
+        private val secureLogger = LoggerFactory.getLogger("secureLogger")
     }
-
 }
