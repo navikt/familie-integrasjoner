a676f12973d68d8e3913a71259af65082a30aa1b
Tor Nærland <tor.nerland@nav.no>
2022-04-25 12:32:24 +0200

    Tilpasser testdata til ny pdl-implementasjon

--- src/test/java/no/nav/familie/integrasjoner/arbeidsfordeling/ArbeidsfordelingServiceTest.kt
+++ src/test/java/no/nav/familie/integrasjoner/arbeidsfordeling/ArbeidsfordelingServiceTest.kt
@@ -7,7 +7,6 @@ import no.nav.familie.integrasjoner.client.rest.PdlRestClient
 import no.nav.familie.integrasjoner.egenansatt.EgenAnsattService
 import no.nav.familie.integrasjoner.geografisktilknytning.GeografiskTilknytningDto
 import no.nav.familie.integrasjoner.geografisktilknytning.GeografiskTilknytningType
-import no.nav.familie.integrasjoner.personopplysning.PersonopplysningerService
 import no.nav.familie.integrasjoner.personopplysning.internal.*
 import no.nav.familie.integrasjoner.personopplysning.internal.ADRESSEBESKYTTELSEGRADERING.FORTROLIG
 import no.nav.familie.integrasjoner.personopplysning.internal.ADRESSEBESKYTTELSEGRADERING.STRENGT_FORTROLIG
@@ -87,7 +86,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente uten diskresjonskode eller egenansatt når ingen er dette`() {
-        mockPersonInfo(emptySet(), emptySet(), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), null, false) }
@@ -95,7 +94,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når søker er kode 6`() {
-        mockPersonInfo(kode6 = setOf(ident), emptySet(), emptySet())
+        mockPersonInfo(mapOf(ident to STRENGT_FORTROLIG))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPSF", false) }
@@ -103,7 +102,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når søker er kode 7`() {
-        mockPersonInfo(emptySet(), kode7 = setOf(ident), emptySet())
+        mockPersonInfo(mapOf(ident to FORTROLIG))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPFO", false) }
@@ -111,7 +110,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med egenAnsatt når søker er egen ansatt`() {
-        mockPersonInfo(emptySet(), emptySet(), egenAnsatte = setOf(ident))
+        mockPersonInfo(mapOf(ident to UGRADERT), egenAnsatte = setOf(ident))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), null, true) }
@@ -119,7 +118,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når barn er kode 6`() {
-        mockPersonInfo(kode6 = setOf(barnXIdent), emptySet(), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT), mapOf(barnXIdent to STRENGT_FORTROLIG))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPSF", false) }
@@ -127,7 +126,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når ektefelle er kode 6`() {
-        mockPersonInfo(kode6 = setOf(ektefelleIdent), emptySet(), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT), ektefelle = ektefelleIdent to STRENGT_FORTROLIG)
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPSF", false) }
@@ -135,7 +134,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når annenForelder er kode 6`() {
-        mockPersonInfo(kode6 = setOf(annenForelderZIdent), emptySet(), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT), ektefelle = annenForelderZIdent to STRENGT_FORTROLIG)
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPSF", false) }
@@ -143,7 +142,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når barn er kode 7`() {
-        mockPersonInfo(emptySet(), kode7 = setOf(barnXIdent), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT), mapOf(barnXIdent to FORTROLIG))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPFO", false) }
@@ -151,7 +150,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når ektefelle er kode 7`() {
-        mockPersonInfo(emptySet(), kode7 = setOf(ektefelleIdent), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT), ektefelle = ektefelleIdent to FORTROLIG)
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPFO", false) }
@@ -159,7 +158,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med diskresjonskode når annenForelder er kode 7`() {
-        mockPersonInfo(emptySet(), kode7 = setOf(annenForelderXIdent), emptySet())
+        mockPersonInfo(mapOf(ident to UGRADERT), ektefelle = annenForelderXIdent to FORTROLIG)
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPFO", false) }
@@ -167,7 +166,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med egenAnsatt når barn er egen ansatt`() {
-        mockPersonInfo(emptySet(), emptySet(), egenAnsatte = setOf(barnXIdent))
+        mockPersonInfo(mapOf(ident to UGRADERT), mapOf(barnXIdent to UGRADERT), egenAnsatte = setOf(barnXIdent))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), null, true) }
@@ -175,7 +174,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med egenAnsatt når ektefelle er egen ansatt`() {
-        mockPersonInfo(emptySet(), emptySet(), egenAnsatte = setOf(ektefelleIdent))
+        mockPersonInfo(mapOf(ident to UGRADERT),  ektefelle = ektefelleIdent to UGRADERT, egenAnsatte = setOf(ektefelleIdent))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), null, true) }
@@ -183,7 +182,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med egenAnsatt når annenForelder er egen ansatt`() {
-        mockPersonInfo(emptySet(), emptySet(), egenAnsatte = setOf(annenForelderXIdent))
+        mockPersonInfo(mapOf(ident to UGRADERT), ektefelle = annenForelderXIdent to UGRADERT, egenAnsatte = setOf(annenForelderXIdent))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), null, true) }
@@ -192,7 +191,7 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med egenAnsatt når annenForelder er egen ansatt og barn er kode7`() {
-        mockPersonInfo(emptySet(), kode7 = setOf(barnXIdent), egenAnsatte = setOf(annenForelderXIdent))
+        mockPersonInfo(mapOf(ident to UGRADERT), mapOf(barnXIdent to FORTROLIG), annenForelderXIdent to UGRADERT, egenAnsatte = setOf(annenForelderXIdent))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), null, true) }
@@ -200,68 +199,116 @@ internal class ArbeidsfordelingServiceTest {
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med strengeste diskresjonskode når alle er representert`() {
-        mockPersonInfo(kode6 = setOf(ektefelleIdent), kode7 = setOf(barnXIdent), egenAnsatte = setOf(annenForelderXIdent))
+        mockPersonInfo(mapOf(ident to UGRADERT), mapOf(barnXIdent to FORTROLIG), annenForelderXIdent to STRENGT_FORTROLIG, egenAnsatte = setOf(annenForelderXIdent))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
-        verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPSF", false) }
+        verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPSF", true) }
     }
 
     @Test
     fun `EnhetForPersonMedRelasjoner - skal hente med både diskresjonskode 7 og egenansatt når én person er dette`() {
-        mockPersonInfo(emptySet(), kode7 = setOf(ektefelleIdent, annenForelderXIdent), egenAnsatte = setOf(annenForelderXIdent))
+        mockPersonInfo(mapOf(ident to UGRADERT), ektefelle = ektefelleIdent to FORTROLIG, egenAnsatte = setOf(ektefelleIdent))
         arbeidsfordelingService.finnBehandlendeEnhetForPersonMedRelasjoner(ident, Tema.ENF)
 
         verify { restClient.finnBehandlendeEnhetMedBesteMatch(any(), any(), "SPFO", true) }
     }
 
-    private fun mockPersonInfo(kode6: Set<String>, kode7: Set<String>, egenAnsatte: Set<String>) {
-        val hovedPerson = "1" to
-                lagPdlPersonMedRelasjoner(familierelasjoner = listOf(PdlForelderBarnRelasjon("2",
-                    FORELDERBARNRELASJONROLLE.BARN)),
-                    sivilstand = listOf(Sivilstand(SIVILSTAND.GIFT, "3")),
-                    fullmakt = listOf(Fullmakt("4")))
-        val barn = lagPdlPersonMedRelasjoner(familierelasjoner = listOf(PdlForelderBarnRelasjon("22",
-            FORELDERBARNRELASJONROLLE.FAR)))
-
-        val ektefelle = Person(fødselsdato = "1980-05-12",
-            navn = "Kari Normann",
-            kjønn = "KVINNE",
-            familierelasjoner = setOf(Familierelasjon(personIdent = Personident(id = "12345678910"),
-                relasjonsrolle = "BARN")),
-            adressebeskyttelseGradering = utledAdressebeskyttelse(ektefelleIdent, kode6, kode7),
-            sivilstand = SIVILSTAND.UGIFT)
-        val fullmakt = PersonMedAdresseBeskyttelse(fullmaktIdent, utledAdressebeskyttelse(fullmaktIdent, kode6, kode7))
-        val barnX = PersonMedAdresseBeskyttelse(barnXIdent, utledAdressebeskyttelse(barnXIdent, kode6, kode7))
-        val barnZ = PersonMedAdresseBeskyttelse(barnZIdent, utledAdressebeskyttelse(barnZIdent, kode6, kode7))
-        val annenForelderX =
-                PersonMedAdresseBeskyttelse(annenForelderXIdent, utledAdressebeskyttelse(annenForelderXIdent, kode6, kode7))
-        val annenForelderZ =
-                PersonMedAdresseBeskyttelse(annenForelderZIdent, utledAdressebeskyttelse(annenForelderZIdent, kode6, kode7))
-
+    private fun mockPersonInfo(
+        Personer: Map<String, ADRESSEBESKYTTELSEGRADERING>,
+        barna: Map<String, ADRESSEBESKYTTELSEGRADERING> = emptyMap(),
+        ektefelle: Pair<String, ADRESSEBESKYTTELSEGRADERING> = ektefelleIdent to UGRADERT,
+        egenAnsatte: Set<String> = emptySet()
+    ) {
+        for (person in Personer) {
+            val hovedPerson = person.key to
+                    lagPdlPersonMedRelasjoner(
+                        familierelasjoner = barna.keys.map {
+                            PdlForelderBarnRelasjon(
+                                it,
+                                FORELDERBARNRELASJONROLLE.BARN
+                            )
+                        }.toMutableList()
+                            .also {
+                                it.add(PdlForelderBarnRelasjon(ektefelle.first, FORELDERBARNRELASJONROLLE.FAR))
+                            },
+                        sivilstand = listOf(Sivilstand(SIVILSTAND.GIFT, "3")),
+                        fullmakt = listOf(Fullmakt("4")),
+                        adressebeskyttelse = listOf(Adressebeskyttelse(person.value))
+                    )
 
 
-        every { pdlRestClient.hentPersonMedRelasjonerOgAdressebeskyttelse(any(), any()) } answers {
-            firstArg<List<String>>().map { hovedPerson }.toMap()
+            every { pdlRestClient.hentPersonMedRelasjonerOgAdressebeskyttelse(listOf(person.key), any()) } returns mapOf(
+                hovedPerson
+            )
+
+            every { pdlRestClient.hentAdressebeskyttelse(person.key, Tema.ENF) } returns PdlPersonMedAdressebeskyttelse(
+                PdlAdressebeskyttelse(
+                    listOf(
+                        Adressebeskyttelse(person.value)
+                    )
+                )
+            )
+
+            for (barn in barna) {
+                val barnPerson = barn.key to
+                        lagPdlPersonMedRelasjoner(
+                            familierelasjoner = listOf(
+                                PdlForelderBarnRelasjon(
+                                    person.key,
+                                    FORELDERBARNRELASJONROLLE.MOR
+                                )
+                            ),
+                            sivilstand = listOf(Sivilstand(SIVILSTAND.GIFT, "3")),
+                            fullmakt = listOf(Fullmakt("4"))
+                        )
+
+                every { pdlRestClient.hentPersonMedRelasjonerOgAdressebeskyttelse(listOf(barn.key), any()) } returns mapOf(
+                    barnPerson
+                )
+
+                every { pdlRestClient.hentAdressebeskyttelse(barn.key, Tema.ENF) } returns PdlPersonMedAdressebeskyttelse(
+                    PdlAdressebeskyttelse(
+                        listOf(
+                            Adressebeskyttelse(barn.value)
+                        )
+                    )
+                )
+            }
+
+            val ektefellePerson = ektefelle.first to
+                    lagPdlPersonMedRelasjoner(
+                        familierelasjoner = listOf(
+                            PdlForelderBarnRelasjon(
+                                person.key,
+                                FORELDERBARNRELASJONROLLE.MOR
+                            )
+                        ),
+                        sivilstand = listOf(Sivilstand(SIVILSTAND.GIFT, "3")),
+                        fullmakt = listOf(Fullmakt("4"))
+                    )
+            every { pdlRestClient.hentPersonMedRelasjonerOgAdressebeskyttelse(listOf(ektefelle.first), any()) } returns mapOf(
+                ektefellePerson
+            )
+
+            every { pdlRestClient.hentAdressebeskyttelse(ektefelle.first, Tema.ENF) } returns PdlPersonMedAdressebeskyttelse(
+                PdlAdressebeskyttelse(
+                    listOf(
+                        Adressebeskyttelse(ektefelle.second)
+                    )
+                )
+            )
         }
-
-
         every { egenAnsattService.erEgenAnsatt(any<Set<String>>()) } returns egenAnsatte.associateWith { true }
 
         every {
             pdlRestClient.hentGeografiskTilknytning(any(), any())
-        } returns GeografiskTilknytningDto(gtType = GeografiskTilknytningType.KOMMUNE,
-                                           gtBydel = null,
-                                           gtKommune = "3032",
-                                           gtLand = null
+        } returns GeografiskTilknytningDto(
+            gtType = GeografiskTilknytningType.KOMMUNE,
+            gtBydel = null,
+            gtKommune = "3032",
+            gtLand = null
         )
-    }
 
-    fun utledAdressebeskyttelse(personIdent: String, kode6: Set<String>, kode7: Set<String>): ADRESSEBESKYTTELSEGRADERING? {
-        return when {
-            kode6.contains(personIdent) -> STRENGT_FORTROLIG
-            kode7.contains(personIdent) -> FORTROLIG
-            else -> UGRADERT
-        }
     }
 
     private fun lagPdlPersonMedRelasjoner(familierelasjoner: List<PdlForelderBarnRelasjon> = emptyList(),
