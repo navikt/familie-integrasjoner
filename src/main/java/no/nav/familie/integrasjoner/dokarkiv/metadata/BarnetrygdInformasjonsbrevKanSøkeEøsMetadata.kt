86c9104f40e13b15bf8797c2037336b25e338ea8
halvormundal <halvorbmundal@gmail.com>
2022-05-09 15:22:04 +0200

    Legg til brev

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInformasjonsbrevKanSøkeEøsMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdInformasjonsbrevKanSøkeEøsMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_INFORMASJONSBREV_KAN_SØKE_EØS
+    override val tittel: String = "Informasjonsbrev kan søke eøs"
+    override val brevkode: String = "Informasjonsbrev kan søke eøs"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVarselOmRevurderingFraNasjonalTilEøsMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdVarselOmRevurderingFraNasjonalTilEøsMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_VARSEL_OM_REVURDERING_FRA_NASJONAL_TIL_EØS
+    override val tittel: String = "Varsler om revurdering fra nasjonal til EØS"
+    override val brevkode: String = "Varsel om revurdering fra nasjonal til EØS"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
