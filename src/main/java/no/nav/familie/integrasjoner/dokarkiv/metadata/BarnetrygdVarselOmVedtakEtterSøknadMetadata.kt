ae873f1c0da8fd76aad14009603f3df7b7984ef7
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-05-13 14:31:44 +0200

    Legg til brev (#633)
    * Legg til brev
    
    * Bruk bindestrek-case på brevkode
    
    * Update src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInformasjonsbrevKanSøkeEøsMetadata.kt
    
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>
    
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVarselOmVedtakEtterSøknadMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdVarselOmVedtakEtterSøknadMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_VARSEL_OM_VEDTAK_ETTER_SØKNAD_I_SED
+    override val tittel: String = "Varsel om vedtak etter søknad i SED"
+    override val brevkode: String = "varsel-om-vedtak-etter-søknad-i-sed"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
