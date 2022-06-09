f3ff97aea2fb529689e10e3b5adf1ac3b81141a0
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2022-06-09 12:17:51 +0200

    Skal støtte å ta imot distribusjonstidspunkt og distribusjonstype for… (#644)
    * Skal støtte å ta imot distribusjonstidspunkt og distribusjonstype for distrubusjon av dokumenter, men skal også være bakoverkompatibel og sende avgårde null for type og KJERNETID for tidspunkt som default. Dette må justeres før juli
    
    * Oppdatert kontrakter med nye typer for distribusjonstidspunkt og distribusjonstype

--- src/main/java/no/nav/familie/integrasjoner/dokdist/DokdistService.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokdist/DokdistService.kt
@@ -15,6 +15,9 @@ class DokdistService(val dokdistRestClient: DokdistRestClient) {
     private fun mapTilDistribuerJournalpostRequestTo(request: DistribuerJournalpostRequest): DistribuerJournalpostRequestTo {
         return DistribuerJournalpostRequestTo(journalpostId = request.journalpostId,
             bestillendeFagsystem = request.bestillendeFagsystem.name,
-            dokumentProdApp = request.dokumentProdApp)
+            dokumentProdApp = request.dokumentProdApp,
+            distribusjonstidspunkt = request.distribusjonstidspunkt,
+            distribusjonstype = request.distribusjonstype
+        )
     }
 }
