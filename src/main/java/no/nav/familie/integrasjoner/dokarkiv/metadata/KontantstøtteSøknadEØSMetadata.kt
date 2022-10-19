55ccb75e148cf5c2565882199f0c3f5640418fcd
bragejahren <brage.ekroll.jahren@nav.no>
2022-10-19 10:21:26 +0200

    Bumpet familie-kontrakter og la inn behandlingstema for KontantstøtteSøknadEØSMetadata

--- src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/KontantstøtteSøknadEØSMetadata.kt
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/KontantstøtteSøknadEØSMetadata.kt
@@ -13,7 +13,7 @@ object KontantstøtteSøknadEØSMetadata : Dokumentmetadata {
     override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
     override val fagsakSystem: Fagsystem? = null
     override val tema: Tema = Tema.KON
-    override val behandlingstema: Behandlingstema = Behandlingstema.Kontantstøtte // TODO: Bytte ut med KontantstøtteEØS
+    override val behandlingstema: Behandlingstema = Behandlingstema.KontantstøtteEØS
     override val kanal: String = "NAV_NO"
     override val dokumenttype: Dokumenttype = Dokumenttype.KONTANTSTØTTE_EØS
     override val tittel: String = "Tilleggsskjema ved krav om utbetaling av barnetrygd " +
