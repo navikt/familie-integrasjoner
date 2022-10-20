f806e6f8ab47a65bd1beef87423c64233c76b744
bragejahren <70642183+bragejahren@users.noreply.github.com>
2022-10-20 12:06:12 +0200

    Opprettet Dokumentmetadata for nye kontantstøtte-dokumenttyper (#718)
    * Opprettet Dokumentmetadata for Kontantstøtte dokumenttyper
    
    * Bumpet familie-kontrakter og la inn behandlingstema for KontantstøtteSøknadEØSMetadata
    
    * Oppdatert kontrakter-felles versjon og endret KS til KONT for nye KS-metadata filer
    
    * Lagt inn egne brevkoder for vedtaksbrevene til kontantstøtte

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/KontantstøtteVedtakMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object KontantstøtteVedtakMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.KONT
+    override val tema: Tema = Tema.KON
+    override val behandlingstema: Behandlingstema = Behandlingstema.Kontantstøtte
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.KONTANTSTØTTE_VEDTAK
+    override val tittel: String = "Vedtak om kontantstøtte"
+    override val brevkode: String = "vedtak-om-kontantstøtte"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.VB
+}
