41ba4415a9243bd7574ffc5a50448c78c3300a2f
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-10-25 14:40:25 +0200

    Fjerner helsesjekk TPS

--- src/main/java/no/nav/familie/integrasjoner/helse/PersonV3Helsesjekk.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.familie.integrasjoner.helse
-
-import no.nav.familie.http.health.AbstractHealthIndicator
-import no.nav.familie.integrasjoner.client.soap.PersonSoapClient
-import org.springframework.stereotype.Component
-
-@Component
-internal class PersonV3Helsesjekk(personSoapClient: PersonSoapClient) :
-    AbstractHealthIndicator(personSoapClient, "helsesjekk.personV3")
