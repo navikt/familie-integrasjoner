351954f7a76e96ac219e2a7911f2c8822491d924
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2022-11-29 10:47:22 +0100

    finnOppgaverV3 har vore deprecated i over to år, og ingenting tydar på at han er i bruk lengre (#751)

--- src/main/java/no/nav/familie/integrasjoner/oppgave/OppgaveController.kt
+++ src/main/java/no/nav/familie/integrasjoner/oppgave/OppgaveController.kt
@@ -1,22 +1,18 @@
 package no.nav.familie.integrasjoner.oppgave
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.Ressurs.Companion.success
-import no.nav.familie.kontrakter.felles.Tema
 import no.nav.familie.kontrakter.felles.oppgave.FinnMappeRequest
 import no.nav.familie.kontrakter.felles.oppgave.FinnMappeResponseDto
 import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveRequest
 import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveResponseDto
 import no.nav.familie.kontrakter.felles.oppgave.MappeDto
 import no.nav.familie.kontrakter.felles.oppgave.Oppgave
-import no.nav.familie.kontrakter.felles.oppgave.OppgavePrioritet
 import no.nav.familie.kontrakter.felles.oppgave.OppgaveResponse
 import no.nav.familie.kontrakter.felles.oppgave.OpprettOppgave
 import no.nav.familie.kontrakter.felles.oppgave.OpprettOppgaveRequest
-import no.nav.familie.kontrakter.felles.oppgave.StatusEnum
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -29,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import javax.validation.constraints.Pattern
 
 @RestController
 @ProtectedWithClaims(issuer = "azuread")
@@ -40,12 +35,6 @@ class OppgaveController(private val oppgaveService: OppgaveService) {
     fun hentOppgave(@PathVariable(name = "oppgaveId") oppgaveId: String): ResponseEntity<Ressurs<Oppgave>> {
         val oppgave = oppgaveService.hentOppgave(oppgaveId.toLong())
         return ResponseEntity.ok().body(success(oppgave, "Hent Oppgave OK"))
-    }
-
-    @GetMapping(path = ["/v3"])
-    @Deprecated("Bruk v4 endepunktet")
-    fun finnOppgaverV3(finnOppgaveRequest: FinnOppgaveRequest): Ressurs<DeprecatedFinnOppgaveResponseDto> {
-        return success(oppgaveService.finnOppgaverV3(finnOppgaveRequest))
     }
 
     @PostMapping(path = ["/v4"])
@@ -149,65 +138,3 @@ class OppgaveController(private val oppgaveService: OppgaveService) {
         return ResponseEntity.ok().body(success(OppgaveResponse(oppgaveId = oppgaveId), "Oppdatering av oppgave OK"))
     }
 }
-
-class DeprecatedFinnOppgaveRequest(
-    val tema: String? = null,
-    val behandlingstema: String? = null,
-    val oppgavetype: String? = null,
-    val enhet: String? = null,
-    val saksbehandler: String? = null,
-    val journalpostId: String? = null,
-    val tilordnetRessurs: String? = null,
-    val tildeltRessurs: Boolean? = null,
-    val opprettetFomTidspunkt: String? = null,
-    val opprettetTomTidspunkt: String? = null,
-    val fristFomDato: String? = null,
-    val fristTomDato: String? = null,
-    val aktivFomDato: String? = null,
-    val aktivTomDato: String? = null,
-    val limit: Long? = null,
-    val offset: Long? = null
-)
-
-@Deprecated("Benytt kontrakt")
-data class DeprecatedFinnOppgaveResponseDto(
-    val antallTreffTotalt: Long,
-    val oppgaver: List<DeprecatedOppgave>
-)
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Deprecated("Benytt kontrakt")
-data class DeprecatedOppgave(
-    val id: Long? = null,
-    val tildeltEnhetsnr: String? = null,
-    val endretAvEnhetsnr: String? = null,
-    val opprettetAvEnhetsnr: String? = null,
-    val journalpostId: String? = null,
-    val journalpostkilde: String? = null,
-    val behandlesAvApplikasjon: String? = null,
-    val saksreferanse: String? = null,
-    val bnr: String? = null,
-    val samhandlernr: String? = null,
-    @field:Pattern(regexp = "[0-9]{13}")
-    val aktoerId: String? = null,
-    val orgnr: String? = null,
-    val tilordnetRessurs: String? = null,
-    val beskrivelse: String? = null,
-    val temagruppe: String? = null,
-    val tema: Tema? = null,
-    val behandlingstema: String? = null,
-    val oppgavetype: String? = null,
-    val behandlingstype: String? = null,
-    val versjon: Int? = null,
-    val mappeId: Long? = null,
-    val fristFerdigstillelse: String? = null,
-    val aktivDato: String? = null,
-    val opprettetTidspunkt: String? = null,
-    val opprettetAv: String? = null,
-    val endretAv: String? = null,
-    val ferdigstiltTidspunkt: String? = null,
-    val endretTidspunkt: String? = null,
-    val prioritet: OppgavePrioritet? = null,
-    val status: StatusEnum? = null,
-    private var metadata: MutableMap<String, String>? = null
-)
