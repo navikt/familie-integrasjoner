4f25948a0d178f5fa267afdfea551d66d208a968
Stig Strøm <stig.helge.strom@nav.no>
2023-01-24 12:57:50 +0100

    La til metadata for dokument som manglet (#775)

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdVarselOmRevurderingInstitusjonMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdVarselOmRevurderingInstitusjonMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_VARSEL_OM_REVURDERING_INSTITUSJON
+    override val tittel: String = "Varsel om revurdering - barnetrygd"
+    override val brevkode: String = "varsel-om-revurdering"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
