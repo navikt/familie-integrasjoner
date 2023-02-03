336010775759804930f3fa726dbede3ebaf96a67
ma10s <mattis.janitz@nav.no>
2023-02-03 15:30:58 +0100

    Vil sørge for at vi ikke logger ident til annet enn securelog dersom vi får feil når vi bygger opp cache. ValueRetrievalException har message med "cache-key" som ofte er ident hos oss.

--- src/main/java/no/nav/familie/integrasjoner/config/ApiExceptionHandler.kt
+++ src/main/java/no/nav/familie/integrasjoner/config/ApiExceptionHandler.kt
@@ -9,6 +9,7 @@ import no.nav.security.token.support.client.core.OAuth2ClientException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.apache.commons.lang3.exception.ExceptionUtils
 import org.slf4j.LoggerFactory
+import org.springframework.cache.Cache.ValueRetrievalException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
@@ -89,6 +90,21 @@ class ApiExceptionHandler {
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(failure("""Det oppstod en feil. ${e.message}""", error = e))
+    }
+
+    // ValueRetrievalException har key (ofte ident) i feilmelding -> må logges til securelog
+    @ExceptionHandler(ValueRetrievalException::class)
+    fun handleException(e: ValueRetrievalException): ResponseEntity<Ressurs<Any>> {
+        secureLogger.error("Exception : ", e)
+        logger.error("Cache - ValueRetrievalException, se securelogger")
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(
+                failure(
+                    """Det oppstod en feil av type ValueRetrievalException. Se securelogs i integrasjoner. """,
+                    error = e.cause,
+                ),
+            )
     }
 
     @ExceptionHandler(PdlNotFoundException::class)
