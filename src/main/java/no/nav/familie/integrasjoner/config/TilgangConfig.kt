9d4e3bb46889ab258b94ced1de20ab8e9e543f39
Johan Blomgren <blommish@users.noreply.github.com>
2023-02-21 09:20:33 +0100

    Fjerner @ConstructorBinding då den er unødvendig sammen med configurationProperties

--- src/main/java/no/nav/familie/integrasjoner/config/TilgangConfig.kt
+++ src/main/java/no/nav/familie/integrasjoner/config/TilgangConfig.kt
@@ -2,8 +2,6 @@ package no.nav.familie.integrasjoner.config
 
 import no.nav.familie.integrasjoner.tilgangskontroll.domene.AdRolle
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties("no.nav.security.jwt.tilgang")
-@ConstructorBinding
 class TilgangConfig(val grupper: Map<String, AdRolle>)
