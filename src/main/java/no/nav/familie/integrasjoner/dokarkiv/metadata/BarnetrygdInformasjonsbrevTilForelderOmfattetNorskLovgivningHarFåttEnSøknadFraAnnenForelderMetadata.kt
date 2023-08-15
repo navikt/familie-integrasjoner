ed1543bf86d43e012de6607dc9c5babfbdf80613
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2023-08-15 09:11:22 +0200

    Legg til informasjonsbrev praksisendring eøs metadata (#864)
    * Legg til informasjonsbrev praksisendring eøs metadata
    
    * Fix typo
    
    * Add bindestrek mellom for å gi tittelene litt mer mening

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInformasjonsbrevTilForelderOmfattetNorskLovgivningHarFåttEnSøknadFraAnnenForelderMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdInformasjonsbrevTilForelderOmfattetNorskLovgivningHarFåttEnSøknadFraAnnenForelderMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_INFORMASJONSBREV_TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_FÅTT_EN_SØKNAD_FRA_ANNEN_FORELDER
+    override val tittel: String = "Informasjonsbrev til forelder omfattet norsk lovgivning - har fått en søknad fra annen forelder"
+    override val brevkode: String = "informasjonsbrev-søknad-fra-annen-forelder"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdInformasjonsbrevTilForelderOmfattetNorskLovgivningVarselOmÅrligKontrollMetadata.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdInformasjonsbrevTilForelderOmfattetNorskLovgivningVarselOmÅrligKontrollMetadata : Dokumentmetadata {
+
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_INFORMASJONSBREV_TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_VARSEL_OM_ÅRLIG_KONTROLL
+    override val tittel: String = "Informasjonsbrev til forelder omfattet norsk lovgivning - varsel om årlig kontroll"
+    override val brevkode: String = "informasjonsbrev-varsel-om-årlig-kontroll"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
