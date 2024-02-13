912927dabe72ecce34d52fc89ee177ddb7e49090
halvor <halvorbmundal@gmail.com>
2024-02-13 16:55:02 +0100

    Legg til metadatafil

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/KontantstøtteInformasjonsbrevMuligLovendring.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object KontantstøtteInformasjonsbrevMuligLovendring : Dokumentmetadata {
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.KONT
+    override val tema: Tema = Tema.KON
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.KONTANTSTØTTE_INFORMASJONSBREV_MULIG_LOVENDRING
+    override val tittel: String = "Informasjonsbrev mulig lovendring kontantstøtte"
+    override val brevkode: String = "informasjonsbrev-mulig-lovendring-kontantsoette"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
