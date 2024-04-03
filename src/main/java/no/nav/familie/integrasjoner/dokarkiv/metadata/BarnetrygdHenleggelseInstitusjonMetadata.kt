fa69e7550bdb927ba57502cca78bd74690d78a58
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2024-04-03 19:29:23 +0900

    legg til institusjon henlegge søknad dokumenttype

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/BarnetrygdHenleggelseInstitusjonMetadata.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object BarnetrygdHenleggelseInstitusjonMetadata : Dokumentmetadata {
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.BA
+    override val tema: Tema = Tema.BAR
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETRYGD_HENLEGGE_TRUKKET_SØKNAD_INSTITUSJON
+    override val tittel: String = "Informasjon om henleggelse institusjon - barnetrygd"
+    override val brevkode: String = "henlegge-trukket-soknad-institusjon"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
