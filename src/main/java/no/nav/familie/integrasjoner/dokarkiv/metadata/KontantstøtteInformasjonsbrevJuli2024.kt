6aac65de2ca24f91fb8699a87ae46de1507323ba
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2024-07-22 00:00:22 +0200

    Legger til KONTANTSTØTTE_INFORMASJONSBREV_LOVENDRING_JULI_2024

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/KontantstøtteInformasjonsbrevJuli2024.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object KontantstøtteInformasjonsbrevJuli2024 : Dokumentmetadata {
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.KONT
+    override val tema: Tema = Tema.KON
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.KONTANTSTØTTE_INFORMASJONSBREV_LOVENDRING_JULI_2024
+    override val tittel: String = "Kontantstøtte – viktig informasjon"
+    override val brevkode: String = "kontantstoette-infobrev-juli-2024"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
