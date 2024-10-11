cc68b642061f24d159e2f4644c324ab403118d17
bragejahren <brage.ekroll.jahren@nav.no>
2024-10-11 09:23:26 +0200

    Setter opp integrasjon mot familie-baks-mottak

---
+++ src/main/java/no/nav/familie/integrasjoner/client/rest/MottakRestClient.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.integrasjoner.client.rest
+
+import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.kontrakter.felles.Tema
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@Service
+class MottakRestClient(
+    @Value("\${FAMILIE_BAKS_MOTTAK_URL}") val mottakBaseUrl: URI,
+    @Qualifier("jwtBearer") val restTemplate: RestOperations,
+) : AbstractRestClient(restTemplate, "baks-mottak") {
+    fun hentPersonerIDigitalSøknad(
+        tema: Tema,
+        journalpostId: String,
+    ): List<String> {
+        val uri =
+            UriComponentsBuilder
+                .fromUri(mottakBaseUrl)
+                .pathSegment("soknad/hent-personer-i-digital-soknad/$tema/$journalpostId")
+                .build()
+                .toUri()
+        return getForEntity(uri)
+    }
+}
