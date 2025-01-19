6aab7702a102f5cbc31eaf6568dcb292f0d6c928
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-01-19 10:34:46 +0100

    Vi går bort fra å bruke sts mot aareg.

--- src/main/java/no/nav/familie/integrasjoner/client/rest/AaregRestClient.kt
+++ src/main/java/no/nav/familie/integrasjoner/client/rest/AaregRestClient.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.integrasjoner.client.rest
 
 import no.nav.familie.http.client.AbstractPingableRestClient
-import no.nav.familie.http.sts.StsRestClient
 import no.nav.familie.integrasjoner.aareg.domene.Arbeidsforhold
 import no.nav.familie.integrasjoner.felles.OppslagException
 import no.nav.familie.log.NavHttpHeaders
@@ -23,9 +22,7 @@ class AaregRestClient(
     @Value("\${AAREG_URL}")
     private val aaregUrl: URI,
     @Qualifier("sts") restOperations: RestOperations,
-    private val stsRestClient: StsRestClient,
 ) : AbstractPingableRestClient(restOperations, "aareg") {
-    // NAV_CALLID og NAV_CONSUMER_ID trengs for kall til ping
     override val pingUri: URI = URI.create("$aaregUrl/$PATH_PING")
 
     fun hentArbeidsforhold(
@@ -64,7 +61,6 @@ class AaregRestClient(
         HttpHeaders().apply {
             add(NavHttpHeaders.NAV_PERSONIDENT.asString(), personIdent)
             add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            add("Nav-Consumer-Token", "Bearer ${stsRestClient.systemOIDCToken}")
         }
 
     companion object {
