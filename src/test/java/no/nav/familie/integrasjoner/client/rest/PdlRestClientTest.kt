e4ce1d64a3072ba4bb1fb0aa47e4398f5f92c5f1
bragejahren <brage.ekroll.jahren@nav.no>
2025-03-11 16:10:19 +0100

    Kaster OppslagException dersom PDL er nede i PdlClientCredentialRestClient og PdlRestClient

--- src/test/java/no/nav/familie/integrasjoner/client/rest/PdlRestClientTest.kt
+++ src/test/java/no/nav/familie/integrasjoner/client/rest/PdlRestClientTest.kt
@@ -1,13 +1,30 @@
 package no.nav.familie.integrasjoner.client.rest
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.integrasjoner.felles.OppslagException
 import no.nav.familie.integrasjoner.geografisktilknytning.PdlHentGeografiskTilknytning
+import no.nav.familie.integrasjoner.personopplysning.internal.PdlPersonMedAdressebeskyttelse
+import no.nav.familie.integrasjoner.personopplysning.internal.PdlPersonRequest
 import no.nav.familie.integrasjoner.personopplysning.internal.PdlResponse
+import no.nav.familie.kontrakter.felles.Tema
 import no.nav.familie.kontrakter.felles.objectMapper
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.RestOperations
+import org.springframework.web.client.exchange
+import java.net.URI
 
 class PdlRestClientTest {
+    private val restOperations: RestOperations = mockk()
+    private val pdlRestClient: PdlRestClient = PdlRestClient(pdlBaseUrl = URI.create("pdl"), restTemplate = restOperations)
+
     @Test
     fun `skal parse harUnathorizedFeil`() {
         val response: PdlResponse<PdlHentGeografiskTilknytning> = objectMapper.readValue<PdlResponse<PdlHentGeografiskTilknytning>>(jsonFeilmelding)
@@ -15,6 +32,26 @@ class PdlRestClientTest {
         assertThat(response.harUnauthorizedFeil()).isTrue()
         val detaljertFeilmeldingFraPdl = response.errors?.joinToString { it.extensions.toString() }
         assertThat(detaljertFeilmeldingFraPdl).contains("adressebeskyttelse_strengt_fortrolig_adresse")
+    }
+
+    @Nested
+    inner class HentAdressebeskyttelse {
+        @Test
+        fun `skal kaste OppslagException hvis kall mot PDL feiler`() {
+            // Arrange
+            every { restOperations.exchange<PdlResponse<PdlPersonMedAdressebeskyttelse>>(any<URI>(), eq(HttpMethod.POST), any<HttpEntity<PdlPersonRequest>>()) } throws RuntimeException("Noe gikk galt")
+
+            // Act & Assert
+            val oppslagException =
+                assertThrows<OppslagException> {
+                    pdlRestClient.hentAdressebeskyttelse("12345678910", Tema.BAR)
+                }
+
+            assertThat(oppslagException.message).isEqualTo("Feil ved henting av adressebeskyttelse")
+            assertThat(oppslagException.kilde).isEqualTo("PdlRestClient.hentAdressebeskyttelse")
+            assertThat(oppslagException.level).isEqualTo(OppslagException.Level.MEDIUM)
+            assertThat(oppslagException.httpStatus).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     /**
