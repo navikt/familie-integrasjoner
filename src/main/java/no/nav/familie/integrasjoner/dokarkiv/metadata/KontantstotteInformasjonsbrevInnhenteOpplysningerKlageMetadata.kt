c12189c888d8bdd69b2dc23e93a2560366caabd7
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-03-28 13:40:13 +0100

    Nytt brev: Innhente opplysninger klage (#1083)
    * Nytt brev: Innhente opplysnigner klage
    
    * Fiks typo
    
    * Fjern deprekert env-param i deploys: APIKEY

---
+++ src/main/java/no/nav/familie/integrasjoner/dokarkiv/metadata/KontantstotteInformasjonsbrevInnhenteOpplysningerKlageMetadata.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.integrasjoner.dokarkiv.metadata
+
+import no.nav.familie.integrasjoner.dokarkiv.client.domene.JournalpostType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Fagsystem
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import org.springframework.stereotype.Component
+
+@Component
+object KontantstotteInformasjonsbrevInnhenteOpplysningerKlageMetadata : Dokumentmetadata {
+    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val fagsakSystem: Fagsystem = Fagsystem.KONT
+    override val tema: Tema = Tema.KON
+    override val behandlingstema: Behandlingstema? = null
+    override val kanal: String? = null
+    override val dokumenttype: Dokumenttype = Dokumenttype.KONTANTSTØTTE_INFORMASJONSBREV_INNHENTE_OPPLYSNINGER_KLAGE
+    override val tittel: String = "Innhente opplysninger klage - kontantstøtte"
+    override val brevkode: String = "innhente-opplysninger-klage-ks"
+    override val dokumentKategori: Dokumentkategori = Dokumentkategori.B
+}
