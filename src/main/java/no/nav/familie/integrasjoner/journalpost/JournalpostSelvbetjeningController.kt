f73010e7e53615b9f3c57606e9dd65d6bcac76e8
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-05-28 10:19:45 +0200

    Sett opp skall for implementasjon av selvbetjente journalposter

--- src/main/java/no/nav/familie/integrasjoner/journalpost/JournalpostSelvbetjeningController.kt
+++ src/main/java/no/nav/familie/integrasjoner/journalpost/JournalpostSelvbetjeningController.kt
@@ -1,7 +1,5 @@
 package no.nav.familie.integrasjoner.journalpost
 
-import no.nav.familie.kontrakter.felles.Ressurs
-import no.nav.familie.kontrakter.felles.Ressurs.Companion.success
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.RequiredIssuers
@@ -11,11 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/sluttbrukerdokument")
+@RequestMapping("/api/journalpostselvbetjening")
 @RequiredIssuers(
     ProtectedWithClaims(issuer = EksternBrukerUtils.ISSUER_TOKENX, claimMap = ["acr=Level4"]),
 )
-class SluttbrukerDokumentController {
-    @GetMapping
-    fun hello(): ResponseEntity<Ressurs<String>> = ResponseEntity.ok(success("Hello", "OK"))
+class JournalpostSelvbetjeningController {
+    @GetMapping("/hello")
+    fun hello(): ResponseEntity<String> = ResponseEntity.ok("Hello")
+
+//    TODO: Implementer
+//    @GetMapping()
+//    fun hentJournalposter(): Ressurs<List<Journalpost>> = journalpostService.finnJournalposter()
 }
