f6e984afbfd338915ed632d2b2d4a31b49f896cb
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-08-19 12:09:30 +0200

    Utvider organisasjonskontrakten til å ha med kommumenummer

--- src/test/java/no/nav/familie/integrasjoner/organisasjon/OrganisasjonTestConfig.kt
+++ src/test/java/no/nav/familie/integrasjoner/organisasjon/OrganisasjonTestConfig.kt
@@ -5,6 +5,7 @@ import io.mockk.mockk
 import no.nav.familie.integrasjoner.client.rest.HentOrganisasjonResponse
 import no.nav.familie.integrasjoner.client.rest.Navn
 import no.nav.familie.integrasjoner.client.rest.OrganisasjonRestClient
+import no.nav.familie.kontrakter.felles.organisasjon.OrganisasjonAdresse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -20,6 +21,7 @@ class OrganisasjonTestConfig {
         val organisasjon =
             HentOrganisasjonResponse(
                 navn = Navn("Navn på bedrift"),
+                adresse = OrganisasjonAdresse(type = "Forretningsadresse", kommunenummer = "0301"),
             )
         every { organisasjonRestClient.hentOrganisasjon(any()) } returns organisasjon
 
