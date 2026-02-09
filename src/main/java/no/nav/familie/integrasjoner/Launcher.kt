a33960da1f9b7070ab2ac575e454c7b7da34e909
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-02-09 12:46:36 +0100

    Konvertert Launcher til Kotlin

--- src/main/java/no/nav/familie/integrasjoner/Launcher.kt
+++ src/main/java/no/nav/familie/integrasjoner/Launcher.kt
@@ -1,17 +1,14 @@
-package no.nav.familie.integrasjoner;
+package no.nav.familie.integrasjoner
 
-import no.nav.familie.integrasjoner.config.ApplicationConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
+import no.nav.familie.integrasjoner.config.ApplicationConfig
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration
 
-@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class Launcher {
+@SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
+class Launcher
 
-    public static void main(String... args) {
-        SpringApplication app = new SpringApplicationBuilder(ApplicationConfig.class).build();
-        app.run(args);
-    }
-
+fun main(args: Array<String>) {
+    val app = SpringApplicationBuilder(ApplicationConfig::class.java).build()
+    app.run(*args)
 }
