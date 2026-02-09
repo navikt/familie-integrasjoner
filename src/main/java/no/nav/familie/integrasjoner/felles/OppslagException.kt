31be26f1b82f6ee0ad2f9196469ff81cc53fa06a
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-02-09 12:18:32 +0100

    Konvertert OppslagException til Kotlin

--- src/main/java/no/nav/familie/integrasjoner/felles/OppslagException.kt
+++ src/main/java/no/nav/familie/integrasjoner/felles/OppslagException.kt
@@ -1,64 +1,22 @@
-package no.nav.familie.integrasjoner.felles;
+package no.nav.familie.integrasjoner.felles
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-
-public class OppslagException extends RuntimeException {
-
-
-
-    private HttpStatusCode httpStatus;
-    private String kilde;
-    private String sensitiveInfo;
-    private Level level;
-    private Throwable error;
-
-    public enum Level {
+class OppslagException(
+    message: String,
+    val kilde: String,
+    val level: Level,
+    val httpStatus: HttpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR,
+    val error: Throwable? = null,
+    val sensitiveInfo: String? = null,
+) : RuntimeException(
+        message,
+        error,
+    ) {
+    enum class Level {
         LAV,
         MEDIUM,
-        KRITISK
-    }
-
-    public OppslagException(String message, String kilde, Level level, HttpStatusCode httpStatus, Throwable error, String sensitiveInfo) {
-        super(message, error);
-        this.httpStatus = httpStatus == null ? INTERNAL_SERVER_ERROR : httpStatus;
-        this.kilde = kilde;
-        this.sensitiveInfo = sensitiveInfo;
-        this.level = level;
-        this.error = error;
-    }
-
-    public OppslagException(String message, String kilde, Level level, HttpStatus httpStatus, Throwable error) {
-        this(message, kilde, level, httpStatus, error, null);
-    }
-
-    public OppslagException(String message, String kilde, Level level, HttpStatus httpStatus) {
-        this(message, kilde, level, httpStatus, null, null);
-    }
-
-    public OppslagException(String message, String kilde, Level level) {
-        this(message, kilde, level, null, null, null);
-    }
-
-    public HttpStatusCode getHttpStatus() {
-        return httpStatus;
-    }
-
-    public String getKilde() {
-        return kilde;
-    }
-
-    public String getSensitiveInfo() {
-        return sensitiveInfo;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public Throwable getError() {
-        return error;
+        KRITISK,
     }
 }
