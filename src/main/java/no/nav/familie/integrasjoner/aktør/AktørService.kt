82b027053ec13ffdb2f273922f5a01f3db48acd3
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-02-09 13:02:40 +0100

    Konvertert AktørService til Kotlin

--- src/main/java/no/nav/familie/integrasjoner/aktør/AktørService.kt
+++ src/main/java/no/nav/familie/integrasjoner/aktør/AktørService.kt
@@ -1,30 +1,27 @@
-package no.nav.familie.integrasjoner.aktør;
+package no.nav.familie.integrasjoner.aktør
 
-import no.nav.familie.integrasjoner.client.rest.PdlRestClient;
-import no.nav.familie.kontrakter.felles.Tema;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-
-import static java.util.Objects.requireNonNull;
+import no.nav.familie.integrasjoner.client.rest.PdlRestClient
+import no.nav.familie.kontrakter.felles.Tema
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
 
 @Service
-public class AktørService {
-
-    private static final Logger secureLogger = LoggerFactory.getLogger("secureLogger");
-    private final PdlRestClient pdlRestClient;
-
-    public AktørService(PdlRestClient pdlRestClient) {
-        this.pdlRestClient = pdlRestClient;
+class AktørService(
+    private val pdlRestClient: PdlRestClient,
+) {
+    @Cacheable(value = ["aktør_aktørId_pdl"], unless = "#result == null")
+    fun getPersonIdentFraPdl(
+        aktørId: String,
+        tema: Tema,
+    ): String {
+        val responseFraRegister = pdlRestClient.hentGjeldendePersonident(aktørId, tema)
+        secureLogger.info("Legger aktørid {} med fnr {} i personident-cache", aktørId, responseFraRegister)
+        return responseFraRegister
     }
 
-    @Cacheable(value = "aktør_aktørId_pdl", unless = "#result == null")
-    public String getPersonIdentFraPdl(String aktørId, Tema tema) {
-        requireNonNull(aktørId, "aktørId");
-        String responseFraRegister = pdlRestClient.hentGjeldendePersonident(aktørId, tema);
-        secureLogger.info("Legger aktørid {} med fnr {} i personident-cache", aktørId, responseFraRegister);
-        return responseFraRegister;
+    companion object {
+        private val secureLogger: Logger = LoggerFactory.getLogger("secureLogger")
     }
-
 }
