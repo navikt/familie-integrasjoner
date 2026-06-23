cf01a8e9fa6dc55d6a31ca35659305f8f41eccb7
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-06-23 09:01:39 +0200

    Bytter til hybrid klient for dokarkiv

--- src/main/java/no/nav/familie/integrasjoner/client/rest/DokarkivLogiskVedleggRestClient.kt
+++ src/main/java/no/nav/familie/integrasjoner/client/rest/DokarkivLogiskVedleggRestClient.kt
@@ -21,7 +21,7 @@ class DokarkivLogiskVedleggRestClient(
     @Value("\${DOKARKIV_SCOPE}") scope: String,
     entraIDRestClientFactory: EntraIDRestClientFactory,
 ) {
-    private val restClient = entraIDRestClientFactory.lagOboRestKlient(scope) { SikkerhetsContext.hentJwt().tokenValue!! }
+    private val restClient = entraIDRestClientFactory.lagHybridRestKlient(scope) { SikkerhetsContext.hentJwt().tokenValue!! }
 
     fun opprettLogiskVedlegg(
         dokumentInfoId: String,
