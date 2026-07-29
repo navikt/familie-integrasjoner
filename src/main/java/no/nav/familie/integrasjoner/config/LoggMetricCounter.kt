74fe0e9937b83eb5cec3402193847ac3541665e1
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-07-29 15:29:55 +0200

    Ignorer kjente feil fra tilbake i feil-metrikk for å unngå unødvendige alarmer
    Velger å beholde loggingen i tilfelle det skulle være nyttig

--- src/main/java/no/nav/familie/integrasjoner/config/LoggMetricCounter.kt
+++ src/main/java/no/nav/familie/integrasjoner/config/LoggMetricCounter.kt
@@ -2,12 +2,21 @@ package no.nav.familie.integrasjoner.config
 
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Metrics
+import no.nav.familie.log.mdc.MDCConstants
+import org.slf4j.MDC
 
 val loggFeilCounter = mutableMapOf<String, Counter>()
+
+private val IGNORERTE_KILDER_FOR_TILBAKE = setOf("oppgave.opprettOppgave", "oppgave.oppdaterOppgave")
 
 fun incrementLoggFeil(
     kilde: String,
 ) {
+    // Ignorer kjente feil fra familie-tilbake for å ikke trigge alarmer
+    if (MDC.get(MDCConstants.MDC_CONSUMER_ID) == "familie-tilbake" && kilde in IGNORERTE_KILDER_FOR_TILBAKE) {
+        return
+    }
+
     if (loggFeilCounter[kilde] == null) {
         loggFeilCounter[kilde] = Metrics.counter("logg.feil", "kilde", kilde)
     }
